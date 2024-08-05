11ceb549fc18f10db963f711004412f6e7094a96
Gamze Nur <121685638+gamzenurkoyuncu@users.noreply.github.com>
2024-08-05 10:27:40 +0200

    helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,28 @@
+first_name = 'Bill'
+last_name = 'Gates'
+full_name = first_name + ' ' + last_name
+print(full_name)
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+print(full_name.strip())
+
+age = 21
+age = age + 1
+age += 1
+print(age)
+print(type(age))
+print('Your age is: '+ str(age)) #print only uses str
+
+height = 250.5
+print(height)
+print(type(height))
+print('Your height is: '+str(height))
+# print(type(height))
+Boolean variable
+human= False #if we use '' it will be str
+print(human)
+print(type(human))
+print('Are you a human: '+ str(human))
+
+
