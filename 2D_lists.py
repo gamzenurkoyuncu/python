293475fd4af78290215cb7df471980b833ae4673
Gamze Nur <121685638+gamzenurkoyuncu@users.noreply.github.com>
2024-08-18 21:42:15 +0200

    Create 2D_lists.py

---
+++ 2D_lists.py
@@ -0,0 +1,13 @@
+# 2D lists = a list of lists
+
+drinks = ['coffee', 'soda','tea']
+dinner = ['pizza', 'hamburger', 'hotdog']
+dessert = ['cake', 'ice cream']
+
+food = [drinks, dinner, dessert]
+print(food)
+print(food[0])
+print(food[1])
+print(food[2])
+
+print(food[1][2]) # hotdog
